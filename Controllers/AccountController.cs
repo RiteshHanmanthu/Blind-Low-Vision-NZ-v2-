777b89fc3c7a3530cf7302dc24e40fea8556aeb4
Ritesh Hanmanthu <ac94848@avcol.school.nz>
2020-10-23 13:43:58 +1300

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+
+namespace LowVision.Controllers
+{
+    public class AccountController: Controller  
+    {
+
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+
+            return View();
+        }
+
+        // Other actions
+    }
+}
